0d0f501f5f3b7602a3a5d7516135e03d04fa7603
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2020-12-16 20:50:26 +0200

    hw-5 done

---
+++ js/task-3.js
@@ -0,0 +1,42 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    for (const element of this.items) {
+      if (item === element) {
+        const elementIndex = this.items.indexOf(element);
+        this.items.splice(elementIndex, 1);
+        return;
+      }
+    }
+    console.log('Такого элемента нет в массиве');
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Дирижабль'); // Такого элемента нет в массиве
